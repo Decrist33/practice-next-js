ba5673f5442d92f6aeacb731b9cf47ec6b025671
Decrist33 <shouscrist@gmail.com>
2024-07-08 21:51:23 -0600

    navbar ui finished

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import CartModal from "./CartModal";
 
 const NavIcons = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -16,11 +17,11 @@ const NavIcons = () => {
     if (!isLoggedIn) {
       router.push("/login");
     }
-    setIsProfileOpen((i) => !i);
+    setIsProfileOpen((prev) => !prev);
   };
 
   return (
-    <div className="flex items-center gap-4 xl:gap-6">
+    <div className="flex items-center gap-4 xl:gap-6 relative">
       <Image
         src="/profile.png"
         alt=""
@@ -30,7 +31,7 @@ const NavIcons = () => {
         onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div>
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/">Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
@@ -42,13 +43,20 @@ const NavIcons = () => {
         height={22}
         className="cursor-pointer"
       />
-      <Image
-        src="/cart.png"
-        alt=""
-        width={22}
-        height={22}
-        className="cursor-pointer"
-      />
+      <div className="relative cursor-pointer">
+        <Image
+          src="/cart.png"
+          alt=""
+          width={22}
+          height={22}
+          onClick={() => setIsCartOpen((prev) => !prev)}
+        />
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama flex items-center justify-center rounded-full text-sm text-white">
+          2
+        </div>
+      </div>
+
+      {isCartOpen && <CartModal />}
     </div>
   );
 };
