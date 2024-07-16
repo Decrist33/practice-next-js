02c3dd45270e51eb9d62eed5a61702748a318d13
Decrist33 <shouscrist@gmail.com>
2024-07-15 21:36:33 -0600

    product list v1 added

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,14 +4,35 @@ import Image from "next/image";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="relative w-full h-80">
-        <Image
-          src="https://images.pexels.com/photos/26594049/pexels-photo-26594049/free-photo-of-national-palace-of-pena.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-          alt=""
-          fill
-          sizes="25vw"
-        />
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/26594049/pexels-photo-26594049/free-photo-of-national-palace-of-pena.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/5326566/pexels-photo-5326566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$59</span>
+        </div>
+        <div className="text-sm text-gray-500">Description</div>
+        <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white transition-all ease-in duration-25">
+          Add To Cart
+        </button>
       </Link>
     </div>
   );
